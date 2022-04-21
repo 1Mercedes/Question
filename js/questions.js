1417be59782e7ee99a34def2dcc120c4f0e54691
Your Name <you@example.com>
2022-04-21 10:11:41 +0500

    fin

---
+++ js/questions.js
@@ -0,0 +1,50 @@
+const quiz = [
+    {
+        q: 'CSS flex-direction: hususiyatida qaysi qiymat standart ravishda bo`ladi?',
+        options: ['row', 'row-reverse', 'column-reverse','column'],
+        answer:0
+
+    },
+    {
+        q: 'opacity ning ishlamaydigan qiymatini belgilang?',
+        options: ['hammasi ishlaydi', '.3', '0.3','30px'],
+        answer:3
+
+    },
+    {
+        q: '!!null nimani qaytaradi?',
+        options: ['0', 'true', '1','false', 'xatolikni'],
+        answer:3
+
+    },
+    {
+        q: 'SASS preprotsessori nima uchun mo`ljallangan?',
+        options: ['javascript','css', 'barchasi variantlar uchun','html'],
+        answer:1
+
+    },
+    {
+        q: ' Malum turdagi fayllar local Git omborida hech qachon yaratilmasligini taminlash uchun qaysi faylni sozlash kerak?',
+        options: ['ignore.git','gitignore.txt', 'git.ignore','.gitignore'],
+        answer:3
+
+    },
+    {
+        q: 'Bootstrap 5dagi .container ning eng katta (maksimal) kengligi qancha?',
+        options: ['1296px','1260px', '1320px','1140px'],
+        answer:2
+
+    },
+     {
+        q: 'HTMLda qanaqa qilib rasm qoysa boladi?',
+        options: ['<image>http://site.com/image.jpg</image>','<image source="http://site.com/image.jpg">', '<img>http://site.com/image.jpg</img>','<img src="http://site.com/image.jpg" alt=""'],
+        answer:3
+
+    },
+        {
+        q: 'Quydagi havola manzili qanday turga kiradi: "./pages/page.html"?',
+        options: ['Nisbiy manzil','Mutloq manzil'],
+        answer:0
+
+    },
+]
